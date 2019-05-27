c1aff30f2d3511b454739cba5d250ca67e42f639
Michael <mihendricks1@gmail.com>
2019-05-27 10:40:30 +0200

    imported body parser

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
 import * as database from './util/database';
+import bodyParser from 'body-parser';
 
 // intialize configuration
 dotenv.config();
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", (req, res) => {
   res.send("Hello world!");
