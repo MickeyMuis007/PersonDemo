fd1293cb5d8ec159deda9a5cb102f5c8582bf8f9
Michael <mihendricks1@gmail.com>
2019-05-28 14:44:37 +0200

    remove default load

--- ui/react/src/App.js
+++ ui/react/src/App.js
@@ -5,50 +5,25 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
+
 library.add(faPlus);
 
 class App extends Component {
   state = {
-    persons: [
-      {
-        id: 'kjds-adjie-jfds-dsa1', gender: 'male', name: 'Max',
-        friends:
-          [{ id: 'sdf-sdf-ljdi-adkjf-1', name: 'Mike' },
-          { id: 'sdf-sdf-ljdi-adkjf-2', name: 'John' },
-          { id: 'sdf-sdf-ljdi-adkjf-3', name: 'Adam' }],
-        tags: ['jids', 'jkdd', 'dkie', 'jids']
-      },
-      {
-        id: 'kjds-adjie-jfds-dsa2', gender: 'male', name: 'Manu',
-        friends:
-          [{ id: 'dkjdkl-jfid-ajidfk-1', name: 'Amy' },
-          { id: 'dkjdkl-jfid-ajidfk-2', name: 'Jamie' },
-          { id: 'dkjdkl-jfid-ajidfk-3', name: 'Ben' }],
-        tags: ['jids', 'dddd', 'fadd']
-      },
-      {
-        id: 'kjds-adjie-jfds-dsa3', gender: 'female', name: 'Stephanie',
-        friends: [
-          { id: 'dkj-dijf-dsi-sdi-31', name: 'Graham' },
-          { id: 'dkj-dijf-dsi-sdi-32', name: 'Jonty' }
-        ],
-        tags: ['jids', 'jkdd', 'dkie']
-      }
-    ]
+    persons: []
   }
 
   getPersons = () => {
-    console.log('Here');
     fetch('http://localhost:3001/person')
       .then(result => {
-        console.log('1st', result.ok)
         if(result.ok)
           return result.json();
         return result;
       })
       .then(result => {
+        const personsTake10 = result.slice(1, 10);
         this.setState({
-          persons: result
+          persons: personsTake10
         })
         
       })
@@ -69,7 +44,7 @@ class App extends Component {
       </div>
     );
 
-
+    this.getPersons();
     return (
       <div className="App">
         <header className="App-header">
