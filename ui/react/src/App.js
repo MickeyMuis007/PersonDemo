df94968f3d721f45f704df46e386e5d9a7f15449
Michael <mihendricks1@gmail.com>
2019-05-28 16:30:05 +0200

    Included a route

--- ui/react/src/App.js
+++ ui/react/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'; 
+import Nav from './Nav/Nav';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
@@ -48,10 +49,12 @@ class App extends Component {
         })}
       </div>
     );
+    
 
     return (
       <div className="App">
         <header className="App-header">
+          <Nav />
           <h1>Person Demo</h1>
         </header>
         <div>
