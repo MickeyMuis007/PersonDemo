5a8bc935dcf67ab5c9190ca77edb2b3402e6ee1d
Michael <mihendricks1@gmail.com>
2019-05-29 16:03:57 +0200

    Create Link button for friends

--- ui/react/src/Person/Person.js
+++ ui/react/src/Person/Person.js
@@ -5,6 +5,7 @@ import './Person.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faEye } from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 library.add(faPlus, faEye);
 
@@ -45,7 +46,8 @@ const person = (props) => {
       <div className="container list-container">
         {friends}
         <div className="d-flex justify-content-end">
-          <button className="b-float" data-toggle="tooltip" data-placement="top" title="Add Friend"><FontAwesomeIcon icon="plus" /></button>
+          <Link to={'/'} className="b-float btn btn-success" data-toggle="tooltip" data-placement="top" title="Add Friend">
+            <FontAwesomeIcon icon="plus" /></Link>
         </div>
       </div>
 
