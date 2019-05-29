7a7fb01b1cf73a6025cb8d20568e4c7da0c4e66e
Michael <mihendricks1@gmail.com>
2019-05-29 11:25:26 +0200

    Filtering values on filter click only.

--- ui/react/src/Person/People.js
+++ ui/react/src/Person/People.js
@@ -19,23 +19,48 @@ class People extends Component {
   componentDidMount = () => {
     fetch('http://localhost:3001/person')
       .then(result => {
-        if (result.ok)
-          return result.json();
-        return result;
-      })
-      .then(result => {
-        const personsTake10 = result.slice(1, 10);
-        // const persons = result;
-        this.setState({
-          persons: personsTake10
-        })
-
+        if (result.ok) {
+          return result.json().then(result => {
+            const persons = result.slice(1, 10);
+            this.setState({
+              persons: persons
+            })
+          });
+        }
       })
       .catch(err => console.log(err));
   }
 
   filter = (value) => {
     console.log('filter', value);
+    fetch(`http://localhost:3001${value}`)
+      .then(result => {
+        if (result.ok) {
+          return result.json().then(result => {
+            const persons = result.slice(1, 10);
+            console.log(persons);
+            if (value.includes('/person')) {
+              console.log('persons')
+              this.setState({
+                persons: persons
+              });
+            } else if (value.includes('/popular-tag')) {
+              console.log('popular tags')
+              this.setState({
+                persons: [],
+                tags: result
+              });
+            } else if (value.includes('/most-popular-friend')) {
+              console.log('Most popular friend');
+              this.setState({
+                persons: [],
+                tags: [],
+                friends: []
+              });
+            }
+          });
+        }
+      })
     this.setState({
       filterSelection: value
     })
@@ -49,6 +74,7 @@ class People extends Component {
         {this.state.persons.map((person) => {
           return <Person
             person={person}
+            filter={this.state.filterSelection}
             key={person.id}
           />
         })}
@@ -60,18 +86,18 @@ class People extends Component {
         <h1 className="text-center">People</h1>
         <div className="d-flex justify-content-center">
           <div className="btn-group btn-group-toggle" data-toggle="buttons">
-            <label className={this.state.filterSelection ===  '/person' ? 'btn btn-secondary active': 'btn btn-secondary'}>
-              <input type="radio" name="options" id="option1" autoComplete="off" 
-              onClick={() => this.filter('/person')}/> All</label>
-            <label className={this.state.filterSelection ===  '/popular-tag?gender=female' ? 'btn btn-secondary active': 'btn btn-secondary'}>
-              <input type="radio" name="options" id="option2" autoComplete="off" 
-              onClick={() => this.filter('/popular-tag?gender=female')} /> Most Popular Female Tags</label>
-            <label className={this.state.filterSelection ===  '/popular-tag?gender=male' ? 'btn btn-secondary active': 'btn btn-secondary'}>
+            <label className={this.state.filterSelection === '/person' ? 'btn btn-secondary active' : 'btn btn-secondary'}>
+              <input type="radio" name="options" id="option1" autoComplete="off"
+                onClick={() => this.filter('/person')} /> All</label>
+            <label className={this.state.filterSelection === '/popular-tag?gender=female' ? 'btn btn-secondary active' : 'btn btn-secondary'}>
               <input type="radio" name="options" id="option2" autoComplete="off"
-              onClick={() => this.filter('/popular-tag?gender=male')} /> Most Popular Male Tags</label>
-            <label className={this.state.filterSelection ===  '/most-popular-friend' ? 'btn btn-secondary active': 'btn btn-secondary'}>
+                onClick={() => this.filter('/popular-tag?gender=female')} /> Most Popular Female Tags</label>
+            <label className={this.state.filterSelection === '/popular-tag?gender=male' ? 'btn btn-secondary active' : 'btn btn-secondary'}>
+              <input type="radio" name="options" id="option2" autoComplete="off"
+                onClick={() => this.filter('/popular-tag?gender=male')} /> Most Popular Male Tags</label>
+            <label className={this.state.filterSelection === '/most-popular-friend' ? 'btn btn-secondary active' : 'btn btn-secondary'}>
               <input type="radio" name="options" id="option3" autoComplete="off"
-              onClick={() => this.filter('/most-popular-friend')} /> Most Popular Friend</label>
+                onClick={() => this.filter('/most-popular-friend')} /> Most Popular Friend</label>
           </div>
         </div>
         <div>
