ad755cd503cb9ef74eb32a41a37c18acdbddf843
Michael <mihendricks1@gmail.com>
2019-05-29 14:31:32 +0200

    Create a modal example in friends

--- ui/react/src/Friend/Friend.js
+++ ui/react/src/Friend/Friend.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class Friend extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
     return (
     <div className="container text-center">
       <h1>Friends</h1>
+      <div>
+        <button className="btn btn-primary" onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+         >
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello This is a modal example</h2>
+          <button className="btn btn-primary" onClick={this.closeModal}>close</button>
+         
+        </Modal>
+      </div>
     </div>
     )
   }
--- ui/react/src/Person/Friend/Friend.js
+++ ui/react/src/Person/Friend/Friend.js
@@ -7,10 +7,11 @@ library.add(faEdit, faTrash, faEye);
 const friend = (props) => {
   return (
     <li className="row mb-2">
-    <button className="b-float btn-info" data-toggle="tooltip" data-placement="top" title="View Friend"><FontAwesomeIcon icon="eye" /></button>
-    <button className="b-float btn-primary" data-toggle="tooltip" data-placement="top" title="Edit Friend"><FontAwesomeIcon icon="edit" /></button>
-    <button className="b-float btn-danger" data-toggle="tooltip" data-placement="top" title="Delete Friend"><FontAwesomeIcon icon="trash" /></button>
-      <span className="col-md-3">{props.friend.name}</span> 
+      <button className="b-float btn-info" data-toggle="tooltip" data-placement="top" title="View Friend"><FontAwesomeIcon icon="eye" /></button>
+      <button className="b-float btn-primary" data-toggle="tooltip" data-placement="top" title="Edit Friend"><FontAwesomeIcon icon="edit" /></button>
+      <button className="b-float btn-danger" data-toggle="tooltip" data-placement="top" title="Delete Friend"><FontAwesomeIcon icon="trash" /></button>
+      <span className="col-md-3">{props.friend.name}</span>
+      
     </li>
   )
 }
