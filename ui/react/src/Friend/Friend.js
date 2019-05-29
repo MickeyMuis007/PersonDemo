a4ce582a2061ef89392bc6b338016bd1ba7a350d
Michael <mihendricks1@gmail.com>
2019-05-29 17:47:31 +0200

    Added a example React Bootstrap modal.

--- ui/react/src/Friend/Friend.js
+++ ui/react/src/Friend/Friend.js
@@ -1,81 +1,49 @@
 import React, { Component } from 'react';
-import Modal from 'react-modal';
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
+import { Modal, Button } from 'react-bootstrap';
 
 class Friend extends Component {
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
 
     this.state = {
-      modalIsOpen: false,
-      anotherModalOpen: false
+      show: false,
     };
-
-    this.openModal = this.openModal.bind(this);
-    this.openAnotherModal = this.openAnotherModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
 
-  openModal() {
-    this.setState({ modalIsOpen: true });
+  handleClose() {
+    this.setState({ show: false });
   }
 
-  openAnotherModal() {
-    this.setState({ anotherModalOpen: true });
-  }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
-
-  closeModal() {
-    this.setState({ modalIsOpen: false, anotherModalOpen: false });
+  handleShow() {
+    this.setState({ show: true });
   }
 
   render() {
     return (
-      <div className="container text-center">
-        <h1>Friends</h1>
-        <div>
-          <button className="btn btn-primary" onClick={this.openModal}>Open Modal</button>
-          <Modal
-            isOpen={this.state.modalIsOpen}
-            onAfterOpen={this.afterOpenModal}
-            onRequestClose={this.closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-          >
-            <h2 ref={subtitle => this.subtitle = subtitle}>Hello This is a modal example</h2>
-            <button className="btn btn-primary" onClick={this.closeModal}>close</button>
+      <>
+        <Button variant="primary" onClick={this.handleShow}>
+          Launch demo modal
+        </Button>
 
-          </Modal>
-          <button className="btn btn-primary" onClick={this.openAnotherModal}>Open Another Modal</button>
-          <Modal
-            isOpen={this.state.anotherModalOpen}
-            onAfterOpen={this.afterOpenModal}
-            onRequestClose={this.closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-          >
-            <h2 ref={subtitle => this.subtitle = subtitle}>Opened another modal</h2>
-            <button className="btn btn-primary" onClick={this.closeModal}>close</button>
-
-          </Modal>
-        </div>
-      </div>
-    )
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
   }
 }
 
