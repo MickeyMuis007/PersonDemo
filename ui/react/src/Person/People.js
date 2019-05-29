2b87ffe01f2347568f30abe34ba48cbcb8c8bfe7
Michael <mihendricks1@gmail.com>
2019-05-29 10:21:38 +0200

    Changing active button for filters on click

--- ui/react/src/Person/People.js
+++ ui/react/src/Person/People.js
@@ -10,7 +10,8 @@ class People extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      persons: []
+      persons: [],
+      filterSelection: 'All'
     }
   }
 
@@ -33,6 +34,13 @@ class People extends Component {
       .catch(err => console.log(err));
   }
 
+  filter = (value) => {
+    console.log('filter', value);
+    this.setState({
+      filterSelection: value
+    })
+  }
+
   render() {
     let person = null;
 
@@ -47,20 +55,23 @@ class People extends Component {
       </div>
     );
 
-
     return (
       <div className="container-fluid">
         <h1 className="text-center">People</h1>
         <div className="d-flex justify-content-center">
           <div className="btn-group btn-group-toggle" data-toggle="buttons">
-            <label className="btn btn-secondary active">
-              <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked /> All</label>
-            <label className="btn btn-secondary">
-              <input type="radio" name="options" id="option2" autoComplete="off" /> Most Popular Female Tags</label>
-            <label className="btn btn-secondary">
-              <input type="radio" name="options" id="option2" autoComplete="off" /> Most Popular Male Tags</label>
-            <label className="btn btn-secondary">
-              <input type="radio" name="options" id="option3" autoComplete="off" /> Most Popular Friend</label>
+            <label className={this.state.filterSelection ===  'All' ? 'btn btn-secondary active': 'btn btn-secondary'}>
+              <input type="radio" name="options" id="option1" autoComplete="off" 
+              onClick={() => this.filter('All')}/> All</label>
+            <label className={this.state.filterSelection ===  'Popular Male Tags' ? 'btn btn-secondary active': 'btn btn-secondary'}>
+              <input type="radio" name="options" id="option2" autoComplete="off" 
+              onClick={() => this.filter('Popular Male Tags')} /> Most Popular Female Tags</label>
+            <label className={this.state.filterSelection ===  'Popular Female Tags' ? 'btn btn-secondary active': 'btn btn-secondary'}>
+              <input type="radio" name="options" id="option2" autoComplete="off"
+              onClick={() => this.filter('Popular Female Tags')} /> Most Popular Male Tags</label>
+            <label className={this.state.filterSelection ===  'Popular Friend' ? 'btn btn-secondary active': 'btn btn-secondary'}>
+              <input type="radio" name="options" id="option3" autoComplete="off"
+              onClick={() => this.filter('Popular Friend')} /> Most Popular Friend</label>
           </div>
         </div>
         <div>
