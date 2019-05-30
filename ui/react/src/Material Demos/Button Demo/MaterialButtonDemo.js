c552ef7067fdba61c32f7be8c2d8d79b9ddced33
Michael <mihendricks1@gmail.com>
2019-05-30 15:15:59 +0200

    Add dividers for material button demo's

--- ui/react/src/Material Demos/Button Demo/MaterialButtonDemo.js
+++ ui/react/src/Material Demos/Button Demo/MaterialButtonDemo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container} from '@material-ui/core';
+import { Container, Divider } from '@material-ui/core';
 import FloatingActionButtonZoom from './FloatingActionButtonZoom';
 import FloatingActionButtons from './FloatingActionButton';
 import ButtonSizes from './ButtonSizes';
@@ -14,30 +14,37 @@ class MaterialButtonDemo extends Component {
       <>
         <Container>
           <h1>Material Demo's</h1>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Floating Buttons</h2>
             <FloatingActionButtons />
           </div>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Floating Action Button Zoom</h2>
             <FloatingActionButtonZoom />
           </div>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Button Sizes</h2>
             <ButtonSizes />
           </div>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Icon Label Buttons</h2>
             <IconLabelButtons />
           </div>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Icon Buttons</h2>
             <IconButtons />
           </div>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Button Bases</h2>
             <ButtonBases />
           </div>
+          <Divider style={{backgroundColor: "white"}}/>
           <div className="my-3">
             <h2>Button Router</h2>
             <ButtonRouter />
