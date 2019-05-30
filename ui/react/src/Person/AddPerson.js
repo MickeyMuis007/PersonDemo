e84fbe911c8e2e2902cd0c3d324c92d3ea5b3cf8
Michael <mihendricks1@gmail.com>
2019-05-30 13:46:32 +0200

    Working on adding people with material design

--- ui/react/src/Person/AddPerson.js
+++ ui/react/src/Person/AddPerson.js
@@ -1,58 +1,81 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './AddPerson.css';
-import { Collapse, Button} from 'react-bootstrap';
+import {
+  TextField, FormGroup, ExpansionPanel, Fab,
+  ExpansionPanelSummary, ExpansionPanelDetails, Typography
+} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 class AddPerson extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      open: false,
-    };
-  }
-  
   render() {
-    const { open } = this.state;
-
     return (
       <div>
         <h1 className="text-center">Add Person</h1>
-        <form className="container add-friend-container">
-          <div className="input-group input-group-lg mb-3 col-md-8">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="name">Name</span>
-            </div>
-            <input className="form-control" aria-label="Name" aria-describedby="name" />
-          </div>
-          <div className="input-group input-group-lg mb-3 col-md-8">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="name">Gender</span>
-            </div>
-            <input className="form-control" aria-label="Name" aria-describedby="name" />
-          </div>
-         
-          <div className="input-group input-group-lg mb-3 col-md-8">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="name">Tags</span>
-            </div>
-            <input className="form-control" aria-label="Name" aria-describedby="name" />
-          </div>
-          <Button
-          onClick={() => this.setState({ open: !open })}
-          aria-controls="example-collapse-text"
-          aria-expanded={open}>Add Friend</Button>
-
-          <Collapse in={this.state.open}>
-          <div className="input-group input-group-lg mb-3 col-md-8 mt-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="name">Friends</span>
-            </div>
-            <input className="form-control" aria-label="Name" aria-describedby="name" />
-          </div>
-        </Collapse>
-
+        <form className="container add-friend-container bg-light">
+          <FormGroup>
+            <TextField
+              id="outlined-name"
+              label="Name"
+              className="col-md-6"
+              margin="normal"
+              variant="outlined" />
+          </FormGroup>
+          <FormGroup>
+            <TextField
+              id="outlined-name"
+              label="Gender"
+              className="col-md-6"
+              margin="normal"
+              variant="outlined" />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header">
+                <div className="d-flex justify-content-center">
+                  <Typography className="text-center">Friends</Typography>
+                </div>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <TextField
+                  id="outlined-name"
+                  label="Friend"
+                  margin="normal"
+                  variant="outlined" />
+                <Fab color="primary" aria-label="Add" className="my-3 mx-3">
+                  <AddIcon />
+                </Fab>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header">
+                <div className="d-flex justify-content-center">
+                  <Typography className="text-center">Tags</Typography>
+                </div>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <TextField
+                  id="outlined-name"
+                  label="Tag"
+                  className=""
+                  margin="normal"
+                  variant="outlined" />
+                <Fab color="primary" aria-label="Add" className="my-3 mx-3">
+                  <AddIcon />
+                </Fab>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </FormGroup>
           <div className="d-flex justify-content-end">
             <Link to={'/people'} className="btn btn-primary mx-2 col-md-2" type="submit">Cancel</Link>
             <button className="btn btn-success mx-2 col-md-2" type="submit">Add</button>
