fc880f9d2434ac56d33f606962f66ba4a5de5fa2
Michael <mihendricks1@gmail.com>
2019-05-30 08:59:43 +0200

    Refactored Friend.js

---
+++ ui/react/src/Modals/Person/Tags/EditTagModal.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { Modal, Button, Form, InputGroup, FormControl, Spinner } from 'react-bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faEye } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlus, faEye);
+
+class EditTagModal extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.save = this.save.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+
+    this.state = {
+      show: false,
+      name: props ? props.name || '' : '',
+      editing: false
+    };
+  }
+
+  handleClose() {
+    if (!this.state.editing)
+      this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  save() {
+    this.setState({ editing: true });
+    setTimeout(() => {
+      this.setState({ show: false, editing: false });
+    }, 3000);
+  }
+
+  render() {
+    let editButton = <Button variant="success" onClick={this.save}>Save</Button>;
+    if (this.state.editing) {
+      editButton = (<Button variant="success" disabled>
+        <Spinner
+          as="span"
+          animation="grow"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        Updating <span className="text-primary">{this.state.name}</span>
+      </Button>);
+    }
+
+    return (
+      <>
+        <button onClick={this.handleShow} className="b-float btn-primary" data-toggle="tooltip" data-placement="top"
+          title="Edit Friend"><FontAwesomeIcon icon="edit" /></button>
+        <Form>
+          <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Edit <span className="text-info">{this.state.name}</span></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="name">Name</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  placeholder="Update Friend Name"
+                  aria-label="Friend Name"
+                  aria-describedby="name"
+                  onChange={this.handleNameChange}
+                />
+              </InputGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button disabled={this.state.editing} variant="secondary" onClick={this.handleClose}>Close</Button>
+              {editButton}
+            </Modal.Footer>
+          </Modal>
+        </Form>
+      </>
+    );
+  }
+}
+
+export default EditTagModal
--- ui/react/src/Person/Friend/Friend.js
+++ ui/react/src/Person/Friend/Friend.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash, faEye } from '@fortawesome/free-solid-svg-icons';
-import Popup from 'reactjs-popup';
 import ViewFriendModal from '../../Modals/Person/Friend/ViewFriendModal';
 import EditFriendModal from '../../Modals/Person/Friend/EditFriendModal';
 import DeleteFriendModal from '../../Modals/Person/Friend/DeleteFriendModal';
 
-library.add(faEdit, faTrash, faEye);
 const friend = (props) => {
   const friendName = props.friend ? props.friend.name || '' : '';
 
@@ -19,29 +14,9 @@ const friend = (props) => {
 
   return (
     <li className="row mb-2 text-primary text-center">
-      <Popup trigger={viewFriendButton} modal>
-        <div className="card">
-          <h1 className="card-header bg-info text-white">View</h1>
-          <div className="card-body">
-            {friendName}
-          </div>
-        </div>
-      </Popup>
-      <Popup trigger={editFriendButton} modal>
-        <div>
-          <label className="mx-2">Edit <span className="font-weight-bold font-italic">{friendName}</span></label>
-          <input />
-        </div>
-      </Popup>
-      <Popup trigger={deleteFriendButton} modal isOpen={false}>
-        <div className="text-danger text-center">
-          <div>Are You sure you want to delete <span className="font-weight-bold font-italic text-primary">{friendName}</span>?</div>
-          <div className="d-flex justify-content-center">
-            <button className="btn btn-danger mx-2">Yes</button>
-            <button className="btn btn-info mx-2">No</button>
-          </div>
-        </div>
-      </Popup>
+      {viewFriendButton}
+      {editFriendButton}
+      {deleteFriendButton}
       <span className="col-md-3 text-white">{friendName}</span>
 
     </li>
