3f011f7c781b0404de7a99c42bfe7fe5d677ad28
Michael <mihendricks1@gmail.com>
2019-05-30 15:54:32 +0200

    Styled home page

--- ui/react/src/Home/Home.js
+++ ui/react/src/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import NavigationButtons from './Navigation Buttons/NavigationButtons';
+import { Divider } from '@material-ui/core';
 
 class Home extends Component {
   render() {
     return (
     <div className="container text-center">
-      <h1>Person Demo</h1>
-      <h2>Welcome to Person Demo</h2>
+      <h1 style={{marginBottom: "0px"}}>Welcome to Person Demo</h1>
+      <Divider style={{backgroundColor: "white", marginBottom: "30px", marginTop: "0px"}}/>
       <NavigationButtons />
     </div>
     )
