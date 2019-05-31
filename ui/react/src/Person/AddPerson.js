51bf5649912ec165721f89c3dcfe053c6aa58020
Michael <mihendricks1@gmail.com>
2019-05-31 10:42:30 +0200

    Working on adding multiple friend ideas

--- ui/react/src/Person/AddPerson.js
+++ ui/react/src/Person/AddPerson.js
@@ -4,10 +4,10 @@ import {
   TextField, FormGroup, ExpansionPanel, Fab, FormLabel, Radio, RadioGroup, Button,
   ExpansionPanelSummary, ExpansionPanelDetails, Typography, FormControl, FormControlLabel
 } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
+import { Add as AddIcon } from '@material-ui/icons';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { LinkContainer } from 'react-router-bootstrap';
-
+import FriendListView from './Friend/FriendListView';
 
 class AddPerson extends Component {
   constructor(props, context) {
@@ -36,7 +36,7 @@ class AddPerson extends Component {
 
   handleNameChange(event) {
     console.log("Handle Name Change");
-    const updateModel = {...this.state.model};
+    const updateModel = { ...this.state.model };
     updateModel.name = event.target.value;
     this.setState({
       model: updateModel
@@ -47,7 +47,7 @@ class AddPerson extends Component {
 
   handleGenderChange(event) {
     console.log("Handle gender change");
-    const updateModel = {...this.state.model};
+    const updateModel = { ...this.state.model };
     updateModel.gender = event.target.value;
     this.setState({
       model: updateModel
@@ -57,6 +57,33 @@ class AddPerson extends Component {
   }
 
   render() {
+    const friendInput = (
+      <div>
+        <TextField
+          id="outlined-name"
+          label="Add Friend"
+          margin="normal"
+          className="col-md-8"
+          variant="outlined" />
+        <Fab color="primary" aria-label="Add" className="my-3 mx-3">
+          <AddIcon />
+        </Fab>
+      </div>
+    );
+
+    const tagInput = (
+      <div>
+        <TextField
+          id="outlined-name"
+          label="Add Tag"
+          className="col-md-8"
+          margin="normal"
+          variant="outlined" />
+        <Fab color="primary" aria-label="Add" className="my-3 mx-3">
+          <AddIcon />
+        </Fab>
+      </div>
+    )
     return (
       <div>
         <h1 className="text-center">Add Person</h1>
@@ -90,15 +117,8 @@ class AddPerson extends Component {
                 </div>
               </ExpansionPanelSummary>
               <ExpansionPanelDetails >
-                <TextField
-                  id="outlined-name"
-                  label="Add Friend"
-                  margin="normal"
-                  className="col-md-8"
-                  variant="outlined" />
-                <Fab color="primary" aria-label="Add" className="my-3 mx-3">
-                  <AddIcon />
-                </Fab>
+                {friendInput}
+                <FriendListView />
               </ExpansionPanelDetails>
             </ExpansionPanel>
           </FormGroup>
@@ -113,22 +133,14 @@ class AddPerson extends Component {
                 </div>
               </ExpansionPanelSummary>
               <ExpansionPanelDetails>
-                <TextField
-                  id="outlined-name"
-                  label="Add Tag"
-                  className="col-md-8"
-                  margin="normal"
-                  variant="outlined" />
-                <Fab color="primary" aria-label="Add" className="my-3 mx-3">
-                  <AddIcon />
-                </Fab>
+                {tagInput}
               </ExpansionPanelDetails>
             </ExpansionPanel>
           </FormGroup>
           <div className="d-flex justify-content-end">
             <LinkContainer to={'/people'}>
               <Button variant="outlined" className="mx-2 col-md-2">Cancel</Button>
-              </LinkContainer>
+            </LinkContainer>
             <Button variant="outlined" color="primary" type="submit" className="mx-2 col-md-2">Save</Button>
           </div>
         </form>
