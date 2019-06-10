3a0b5b3fcddded188fef29ddbb2f4cf0e110898b
Michael <mihendricks1@gmail.com>
2019-06-10 10:07:29 +0200

    Completed Blog listing

--- ui/react/src/MDB/Sections/Blog.js
+++ ui/react/src/MDB/Sections/Blog.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
 import { Divider } from '@material-ui/core';
 import BlogList1 from './BlogList1/BlogList1';
+import BlogList2 from './BlogList2/BlogList2';
+import BlogList3 from './BlogList3/BlogList3';
+import BlogList4 from './BlogList4/BlogList4';
 
 class Blog extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <h1 className="text-center">Blog List1</h1>
-        <Divider style={{backgroundColor: "white"}}/>
-        <BlogList1 />
-      </div>
+      <>
+        <div className="container-fluid">
+          <h1 className="text-center">Blog List 1</h1>
+          <Divider style={{ backgroundColor: "white" }} />
+          <BlogList1 />
+        </div>
+        <div className="container-fluid">
+          <h1 className="text-center">Blog List 2</h1>
+          <Divider style={{ backgroundColor: "white" }} />
+          <BlogList2 />
+        </div>
+        <div className="container-fluid">
+          <h1 className="text-center">Blog List 3</h1>
+          <Divider style={{ backgroundColor: "white" }} />
+          <BlogList3 />
+        </div>
+        <div className="container-fluid">
+          <h1 className="text-center">Blog List 4</h1>
+          <Divider style={{ backgroundColor: "white" }} />
+          <BlogList4 />
+        </div>
+      </>
     );
   }
 }
